d23c6a7f118cd06065d656dac8af4b39b9fe95c7
David Zihala <zihaladavid@gmail.com>
2019-05-15 23:11:38 +0200

    Counter deleted

--- scripts/config.py
+++ scripts/config.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import os
 import configparser
 import argparse
 from pathlib import Path
@@ -9,10 +10,12 @@ parser = argparse.ArgumentParser(description='Script for parsing fun.',
 parser.add_argument('-d', '--dataset_folder', required=True)
 parser.add_argument('-i', '--input_file', required=True)
 parser.add_argument('-o', '--orthomcl', help='path to orthomcl if not in dataset_folder')
+# parser.add_argument('-s', '--stable_dataset', help='Dataset for long-term usage.')
 args = parser.parse_args()
 
 if not args.orthomcl:
     args.orthomcl = str(Path(args.dataset_folder, 'orthomcl'))
+
 with open('config.ini', 'w') as configfile:
     config['PATHS'] = {'dataset_folder': args.dataset_folder,
                        'input_file': args.input_file,
