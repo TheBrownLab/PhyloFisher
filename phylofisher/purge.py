f79e31aeaaaa0bc98739507a2af1577437031b53
David Zihala <zihaladavid@gmail.com>
2019-07-11 14:49:26 +0200

    purge

--- phylofisher/purge.py
+++ phylofisher/purge.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import os
 import configparser
+import argparse
 import csv
 from Bio import SeqIO
 from glob import glob
@@ -17,24 +18,34 @@ def fasta_cleaner(file, org_set):
 
 def delete_homologs(org_set):
     for folder in ['orthologs', 'paralogs']:
-        files = glob(os.path.join(dfo, folder))
+        files = glob(os.path.join(dfo, folder) + '/*.fas')
         for file in files:
             fasta_cleaner(file, org_set)
 
 
 def parse_metadata():
-    with open(meta, 'rb') as f:
+    meta = os.path.join(dfo, 'metadata.tsv')
+    with open(meta, 'r') as f:
         reader = csv.reader(f, delimiter='\t')
         lines = list(reader)
         return lines
 
 
-def delete_group_org(orgs=None, groups=None):
+def delete_group_org(orgs_=None, groups=None):
     meta = os.path.join(dfo, 'metadata.tsv')
-    group_set = set(groups.split(','))
+    if groups:
+        group_set = set(groups.split(','))
+    else:
+        group_set = set()
+
+    if orgs_:
+        orgs = set(orgs.split(','))
+    else:
+        orgs = set()
+
     lines = parse_metadata()
     orgs_to_del = set()
-    with open(meta, 'wt') as out_file:
+    with open(meta, 'w') as out_file:
         res = csv.writer(out_file, delimiter='\t')
         for line in lines:
             if line[2] in group_set:
@@ -43,8 +54,7 @@ def delete_group_org(orgs=None, groups=None):
                 orgs_to_del.add(line[0])
             else:
                 res.writerow(line)
-    delete_orgs(orgs_to_del)
-
+    delete_homologs(orgs_to_del)
 
 
 if __name__ == '__main__':
@@ -55,8 +65,8 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description='Script for deleting orgs/taxonomic'
                                                  'groups from the dataset', usage="blabla")
-    parser.add_argument('-o', '--orgs')
-    parser.add_argument('-t', '--tax_groups')
+    parser.add_argument('-o', '--orgs', help='Short names of organisms for deletion: Org1,Org2,Org3')
+    parser.add_argument('-g', '--tax_groups', help='Names of taxonomic groups for deletion: Group1,Group2,Group3')
     args = parser.parse_args()
 
     delete_group_org(args.orgs, args.tax_groups)
