361ae1f930a5910cc6aac18ec528282009993b47
David Zihala <zihaladavid@gmail.com>
2019-07-13 14:06:04 +0200

    len_filter

--- phylofisher/utilities/len_filter.py
+++ phylofisher/utilities/len_filter.py
@@ -1,5 +1,8 @@
+#!/usr/bin/env python
+import os
 from Bio import SeqIO
 import glob
+import argparse
 
 def good_length(trimmed_aln):
     records = list(SeqIO.parse(trimmed_aln, 'fasta'))
@@ -9,10 +12,23 @@ def good_length(trimmed_aln):
             if coverage > 0.3:
                 res.write(f'>{record.description}_{round(coverage, 2)}\n{record.seq}\n')
             else:
-                print(record.name, trimmed_aln)
+                print(record.name, trimmed_aln, coverage)
 
 
-files = glob.glob('*.fas')
 
-for file in files:
-    good_length(file)
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Scriptm which filters short sequences and adds'
+                                                 'information about lenght to all other seqs.',
+                                     usage="len_filter -i <folder_with_trimmed_alignments> [options]")
+    parser.add_argument('-i', '--input', required=True, help='folder with alignments')
+    parser.add_argument('-s', '--sufix', help='Sufix of your alignments. Not required is folder'
+                                             'contains only alignments.')
+    args = parser.parse_args()
+
+    if args.sufix:
+        files = glob.glob(os.path.join(args.input,f'*{args.sufix}'))
+    else:
+        files = glob.glob(os.path.join(args.input,'*'))
+
+    for file in files:
+        good_length(file)
