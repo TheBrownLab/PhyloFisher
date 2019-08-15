c2bc18fedead5fd8fa9d01ee1054f6aef0ba852c
David Zihala <zihaladavid@gmail.com>
2019-08-15 16:43:03 +0200

    contaminations

--- phylofisher/forest.py
+++ phylofisher/forest.py
@@ -41,7 +41,6 @@ def parse_metadata(metadata, input_metadata=None):
 
 
 def suspicious_clades(tree):
-    print(tree)
     t = Tree(tree)
     R = t.get_midpoint_outgroup()
     t.set_outgroup(R)
@@ -88,15 +87,82 @@ def get_best_candidates(tree_file):
     return top_seqs
 
 
-def tree_to_pdf(tree_file):
+def parse_contaminations(file):
+    cont_dict = {}
+    for line in open(file):
+        org, tax, rank = line.split('\t')
+        cont_dict[org] = (tax, rank.strip())
+    return cont_dict
+
+
+def expected_neighborhood(parent, cont_key_rank):
+    keywords = set()
+    key = cont_key_rank[0]
+    rank = cont_key_rank[1]
+    assert rank in ['group', 'subtax', 'org'], f'{rank} has to be group,subtax or org'
+    for org in parent.get_leaf_names():
+        if (org.count('_') != 4):
+            if '..' in org:
+                org = org.split('..')[0]
+            else:
+                org = org.split('_')[0]
+            if rank == 'group':
+                keywords.add(metadata[org]['group'])
+            elif rank == 'subtax':
+                keywords.add(metadata[org]['subtax'])
+            elif rank == 'org':
+                keywords.add(org)
+    if keywords:
+        if len(keywords) == 1:
+            if key in list(keywords)[0]:
+                return True
+            else:
+                return False
+        else:
+            return False
+    return expected_neighborhood(parent.up, cont_key_rank)
+
+
+def check_contamination(node, cont_dict):
+    org = node.name.split('_')[0]
+    if expected_neighborhood(node.up, cont_dict[org]):
+        return True
+    return False
+
+
+def collect_contaminations(tree_file, cont_dict):
+    t = Tree(tree_file)
+    R = t.get_midpoint_outgroup()
+    t.set_outgroup(R)
+    cont_table_name = set()
+
+    contaminations = set()
+    n = 0
+    for node in t.traverse('preorder'):
+        if node.is_leaf() is True:
+            if node.name.count('_') == 4:
+                name = node.name
+                org = name.split('_')[0]
+                quality = f'{node.name.split("_")[-3]}_{node.name.split("_")[-2]}_{node.name.split("_")[-1]}'
+                table_name = f'{metadata[org]["full"]}_{quality}@{org}'
+                if org in cont_dict:
+                    if check_contamination(node, cont_dict) is True:
+                        contaminations.add(name)
+                        cont_table_name.add(table_name)
+        n += 1
+    return contaminations, cont_table_name
+
+
+def tree_to_pdf(tree_file, contaminations=[], backpropagion=None):
     tree_base = str(os.path.basename(tree_file))
     if args.prefix:
         tree_base = tree_base.replace(args.prefix, '')
-    if args.sufix:
-        tree_base = tree_base.replace(args.sufix, '')
+    if args.suffix:
+        tree_base = tree_base.replace(args.suffix, '')
 
     output_base = f"{output_folder}/{tree_base}"
-    table = open(f"{output_folder}/{tree_base}.tsv",'w')
+    if not backpropagion:
+        table = open(f"{output_folder}/{tree_base}.tsv",'w')
 
     top_ranked = get_best_candidates(tree_file)
     t = Tree(tree_file)
@@ -141,34 +207,48 @@ def tree_to_pdf(tree_file):
                 empty_face = TextFace("\t"*20)
                 node.add_face(empty_face, column=2, position = "aligned")
                 node.add_face(empty_face, column=3, position="aligned")
+                original_name = node.name
                 org = node.name
                 if org.count('_') == 4:
                     quality = f'{org.split("_")[-3]}_{org.split("_")[-2]}_{org.split("_")[-1]}'
-                    org = org.split('_')[0]
+                    org = original_name.split('_')[0]
                     group = metadata[org]['group']
-                    node.name = f'{node.name}'
+
+
                     if group in tax_col:
                         tax_name = TextFace(f'[{group} {metadata[org]["subtax"]}]', fgcolor=tax_col[group], bold=True)
                     else:
                         tax_name = TextFace(f'[{group} {metadata[org]["subtax"]}]', bold=True)
                     node.add_face(tax_name, column=1, position = "aligned")
-                    if node.name in top_ranked:
+
+                    if original_name in contaminations:
+                        if not backpropagion:
+                            table.write(f'{metadata[org]["full"]}_{quality}@{org}\t{group}\td\n')
+                        paraf = TextFace(f'{metadata[org]["full"]}_{quality}@{org}', fgcolor='red')
+                        node.name = ''
+                        node.add_face(paraf, column=0)
+                    elif node.name in top_ranked:
                         tname = TextFace(f'{metadata[org]["full"]}_{quality}@{org}', bold=True)
-                        table.write(f'{metadata[org]["full"]}_{quality}@{org}\t{group}\to\n')
+                        if not backpropagion:
+                            table.write(f'{metadata[org]["full"]}_{quality}@{org}\t{group}\to\n')
                         node.name = ''
                         node.add_face(tname, column=0, position='branch-right')
                     else:
-                        table.write(f'{metadata[org]["full"]}_{quality}@{org}\t{group}\tp\n')
+                        if not backpropagion:
+                            table.write(f'{metadata[org]["full"]}_{quality}@{org}\t{group}\tp\n')
                         node.name = f'{metadata[org]["full"]}_{quality}@{org}'
+
+
                 elif '..' in org:
                     org, length = org.split('_')
                     para = org
                     org = org.split('..')[0]
                     group = f"{metadata[org]['group']}"
-                    deletef = TextFace(f'{metadata[org]["full"]}_{length}@{para}', fgcolor='blue')
+                    paraf = TextFace(f'{metadata[org]["full"]}_{length}@{para}', fgcolor='blue')
                     node.name = ''
-                    node.add_face(deletef, column=0)
-                    table.write(f'{metadata[org]["full"]}_{length}@{para}\t{group}\tp\n')
+                    node.add_face(paraf, column=0)
+                    if not backpropagion:
+                        table.write(f'{metadata[org]["full"]}_{length}@{para}\t{group}\tp\n')
                     gface = TextFace(f'[{group} {metadata[org]["subtax"]}]')
                     node.add_face(gface, column=1, position="aligned")
 
@@ -178,7 +258,8 @@ def tree_to_pdf(tree_file):
                     gface = TextFace(f'[{group} {metadata[org]["subtax"]}]') #TODO do not touch me pleeeease
                     color = metadata[org]['col']
                     node_style["bgcolor"] = color
-                    table.write(f'{metadata[org]["full"]}_{length}@{org}\t{group}\to\n')
+                    if not backpropagion:
+                        table.write(f'{metadata[org]["full"]}_{length}@{org}\t{group}\to\n')
                     node.name = f'{metadata[org]["full"]}_{length}@{org}'
                     node.add_face(gface, column=1, position="aligned")
             node.set_style(node_style)
@@ -186,12 +267,11 @@ def tree_to_pdf(tree_file):
     title_face = TextFace(f'<{output_base.split("/")[-1]}, {sus_clades} suspicious clades>',  bold=True)
     ts.title.add_face(title_face, column=1)
     t.render(output_base + '_tree.svg', tree_style=ts)
-    table.close()
+    if not backpropagion:
+        table.close()
 
 
 def trees_plus_table(trees):
-    # with Pool(processes=threads) as pool:
-    #     pool.map(tree_to_pdf, trees)
     with Pool(processes=threads) as pool:
         suspicious = list(pool.map(suspicious_clades, trees))
         return suspicious
@@ -267,6 +347,28 @@ def plot_problematic(problematic_orgs):
             pdf.savefig(bbox_inches='tight')
             plt.close()
 
+
+def backpropagate_contamination(tree_file, cont_names):
+    tree_base = str(os.path.basename(tree_file))
+    if args.prefix:
+        tree_base = tree_base.replace(args.prefix, '')
+    if args.suffix:
+        tree_base = tree_base.replace(args.suffix, '')
+
+    original_table = open(f"{output_folder}/{tree_base}.tsv", 'r').readlines()
+    with open(f"{output_folder}/{tree_base}.tsv", 'w') as res_:
+        n = 0
+        for line in original_table:
+            sline = line.split('\t')
+            name = sline[0]
+            tax = sline[1]
+            status = sline[2].strip()
+            if name in cont_names:
+                status = 'd'
+            n += 1
+            res_.write(f'{name}\t{tax}\t{status}\n')
+
+
 if __name__ == '__main__':
     config = configparser.ConfigParser()
     config.read('config.ini')
@@ -275,38 +377,53 @@ if __name__ == '__main__':
     parser.add_argument('-o', '--output_folder', required=True)
     parser.add_argument('-m', '--metadata', required=True)
     parser.add_argument('-n', '--input_metadata')
+    parser.add_argument('-c', '--contaminations')
+    parser.add_argument('-b', '--backpropagate', action='store_true', help='backpropagate contaminations')
     parser.add_argument('--prefix')
-    parser.add_argument('--sufix')
+    parser.add_argument('--suffix')
     args = parser.parse_args()
     trees_folder = args.trees_folder
     output_folder = args.output_folder
 
-    os.mkdir(output_folder)
+    if not args.backpropagate:
+        os.mkdir(output_folder)
 
     if args.prefix:
         trees = glob.glob(f"{trees_folder}/{args.prefix}*")
-    elif args.sufix:
-        trees = glob.glob(f"{trees_folder}/*{args.sufix}")
+    elif args.suffix:
+        trees = glob.glob(f"{trees_folder}/*{args.suffix}")
     else:
         trees = glob.glob(f"{trees_folder}/*")
 
     number_of_genes = len(trees)
     metadata, tax_col = parse_metadata(args.metadata, args.input_metadata)
     threads = 1 #TODO pararell run
-    suspicious = trees_plus_table(trees)
-    suspicious = sorted(suspicious)
 
-    with open('suspicious.txt', 'w') as res:
-        for file, clades in suspicious:
-            if clades:
-                res.write(f'{file}\n========================\n')
-                for clade_ in clades:
-                    res.write(f'{clade_}\n')
-                res.write(f'\n\n\n')
-    result_clades = [clades[1] for clades in suspicious]
-    nonredundant_clades = nonredundant(result_clades)
-    problematic_orgs = problematic(nonredundant_clades)
-    plot_problematic(problematic_orgs)
-    for tree in trees:
-        print(tree)
-        tree_to_pdf(tree)
+    if not args.backpropagate:
+        suspicious = trees_plus_table(trees)
+        suspicious = sorted(suspicious)
+
+        with open('suspicious.txt', 'w') as res:
+            for file, clades in suspicious:
+                if clades:
+                    res.write(f'{file}\n========================\n')
+                    for clade_ in clades:
+                        res.write(f'{clade_}\n')
+                    res.write(f'\n\n\n')
+        result_clades = [clades[1] for clades in suspicious]
+        nonredundant_clades = nonredundant(result_clades)
+        problematic_orgs = problematic(nonredundant_clades)
+        plot_problematic(problematic_orgs)
+
+    if args.contaminations:
+        cont_dict = parse_contaminations(args.contaminations)
+        for tree in trees:
+            contaminations, contaminated_table = collect_contaminations(tree, cont_dict)
+            if args.backpropagate:
+               backpropagate_contamination(tree, contaminated_table)
+               tree_to_pdf(tree, contaminations, backpropagion=True)
+            else:
+                tree_to_pdf(tree, contaminations)
+    else:
+        for tree in trees:
+            tree_to_pdf(tree)
