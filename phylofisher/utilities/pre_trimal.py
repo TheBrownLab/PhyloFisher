f3db9c1910b81e99d4872a51109f5f0df823b5bb
David Zihala <zihaladavid@gmail.com>
2019-10-22 22:15:02 +0200

    pretrimal

--- phylofisher/utilities/pre_trimal.py
+++ phylofisher/utilities/pre_trimal.py
@@ -3,7 +3,7 @@ import sys
 from Bio import SeqIO
 
 def x_to_dash(file):
-    file_name = f'{file.split(".")[0]}.aln'
+    file_name = f'{file.split(".")[0]}.pre_trimal'
     with open(file_name, 'w') as res:
         for record in SeqIO.parse(file, 'fasta'):
             res.write(f'>{record.name}\n{str(record.seq).replace("X", "-")}\n')
