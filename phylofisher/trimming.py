a59373eb58406c3e1bc33831d4bf1e0c30c054cc
David Zihala <zihaladavid@gmail.com>
2019-10-22 22:02:38 +0200

    pretrimal

--- phylofisher/trimming.py
+++ phylofisher/trimming.py
@@ -23,7 +23,7 @@ def prepare_analyses(dataset, threads):
     divvier = str(Path(dfo, 'lib/Divvier/divvier'))
     command += f'{divvier} -mincol 4 -divvygap {root}.pre_aln && '
 
-    command += f'pre_trimal.py {root}.pre_aln'
+    command += f'pre_trimal.py {root}.pre_aln && '
 
     command += f'trimal -in {root}.aln.divvy.fas -gt 0.2 -out {root}.trimal && '
 
@@ -36,10 +36,10 @@ def prepare_analyses(dataset, threads):
 
     command += f'trimal -in {root}.aln2.divvy.fas -gt 0.01 -out {root}.final && '
 
-    command += f'raxmlHPC-PTHREADS-AVX2 -T {threads} -m PROTGAMMALG4XF -f a -s {root}.final' \
-        f' -n {root}.tre -x 123 -N 100 -p 12345 && '
-
-    command += f'add_aln_length.py {root}\n'
+    # command += f'raxmlHPC-PTHREADS-AVX2 -T {threads} -m PROTGAMMALG4XF -f a -s {root}.final' \
+    #     f' -n {root}.tre -x 123 -N 100 -p 12345 && '
+    #
+    # command += f'add_aln_length.py {root}\n'
 
     return command
 
@@ -48,7 +48,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Script for deleting orgs/taxonomic'
                                                  'groups from the dataset', usage="purge.py [OPTIONS]")
     parser.add_argument('-d', '--dataset_folder')
-    parser.add_argument('-i', '--input_folder', help='Short names of organisms for deletion: Org1,Org2,Org3')
+    parser.add_argument('-i', '--input_folder'  )
     parser.add_argument('-s', '--suffix', default='.fas')
     parser.add_argument('-c', '--use_config', action='store_true')
     parser.add_argument('-t', '--threads', type=int, default=1)
