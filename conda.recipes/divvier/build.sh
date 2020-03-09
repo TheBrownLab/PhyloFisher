5b25517915cea649c1d89acb2b0b3cc1a5e23638
Robert Jones <rej110@msstate.edu>
2020-03-08 19:25:09 -0500

    updated conda recipies

--- conda.recipes/divvier/build.sh
+++ conda.recipes/divvier/build.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
 
 mkdir $PREFIX/Divvier
-cp -r $SRC_DIR/* $PREFIX/Divvier
-
-ln $PREFIX/Divvier/divvier $PREFIX/bin/divvier
+cp -r $SRC_DIR/* $PREFIX/
--- conda.recipes/prequal/build.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-mkdir $PREFIX/Prequal
-cp -r $SRC_DIR/* $PREFIX/Prequal
-
-ln $PREFIX/Prequal/prequal $PREFIX/bin/prequal
