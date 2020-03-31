0c25b3298f963190a3c0770ec4640a03401a2993
David Zihala <zihaladavid@gmail.com>
2020-03-31 20:45:12 +0200

    make alignments

--- phylofisher/utilities/genetic_code.py
+++ phylofisher/utilities/genetic_code.py
@@ -19,8 +19,15 @@ from phylofisher import help_formatter
 plt.style.use('ggplot')
 
 
-def prepare_alignments():
-    pass
+def prepare_alignments(threads):
+    alignmnets_folder = str(Path(dfo, 'alignments'))
+    if os.path.isdir(alignmnets_folder) == False:
+        os.mkdir(alignmnets_folder)
+    for file in glob(str(Path(dfo, f'orthologs/*.fas'))):
+        cmd = f'mafft --auto --reorder --anysymbol --thread {threads} \
+            {file} > {file.replace("orthologs","alignments").split(".")[0]}.aln'
+        print(cmd)
+        subprocess.call(cmd,shell=True)
 
 
 def parse_query(alignment_file, queries_list):
@@ -45,12 +52,11 @@ def collect_queries(queries_list):
                 res.write(parsed_query)
 
 
-def blastDB_tblastn(genome, threads):
-    subprocess.call(f'makeblastdb -in {genome} -dbtype nucl',
+def blastDB_tblastn(trans, threads, evalue):
+    subprocess.call(f'makeblastdb -in {trans} -dbtype nucl',
                     shell=True)
-    subprocess.call(f'tblastn -query queries.fasta -db {genome} -num_threads {threads} \
-                    -evalue 1e-30 -outfmt 5 -out blast_out.xml -max_target_seqs 5 \
-                    -gapopen 11 -gapextend 2', shell=True)
+    subprocess.call(f'tblastn -query queries.fasta -db {trans} -num_threads {threads} \
+                    -evalue {evalue} -outfmt 5 -out blast_out.xml -max_target_seqs 5 ', shell=True)
 
 
 def read_transcriptome(transcriptome):
@@ -74,7 +80,7 @@ def alignment_parsing(alignment_file, query_n):
     return position_dict
 
 
-def stops_against_alignment(transcriptome):
+def collect_counts(transcriptome, conservation_lvl):
     transcriptome_dict = {}
     for sequence in SeqIO.parse(transcriptome, 'fasta'):
         transcriptome_dict[sequence.name] = sequence.seq
@@ -110,27 +116,27 @@ def stops_against_alignment(transcriptome):
                                                 - int(query_seq[:position].count('-')))
 
                             if frame in [1, 2, 3]:
-                                real_genome_position = int(
+                                real_trans_position = int(
                                     hit_start + (3 * (position - int(hit_seq[:position].count('-')))) - 1)
                                 codon = (
-                                transcriptome_dict[hit_name][real_genome_position:real_genome_position + 3]).upper()
+                                transcriptome_dict[hit_name][real_trans_position:real_trans_position + 3]).upper()
                             else:
                                 sequence = transcriptome_dict[hit_name]
-                                real_genome_position = int(len(sequence) - hit_end + (
+                                real_trans_position = int(len(sequence) - hit_end + (
                                             3 * (position - int(hit_seq[:position].count('-')))))
                                 revcom = sequence.reverse_complement()
-                                codon = (revcom[real_genome_position:real_genome_position + 3]).upper()
+                                codon = (revcom[real_trans_position:real_trans_position + 3]).upper()
                             col = str(positional_dict[real_position][1])
                             for amino_acid in AA:
                                 AA_count[amino_acid] = col.count(amino_acid)
                             y = pd.Series(AA_count)
-                            if y.max() > len(col) * 0.7:
+                            if y.max() > len(col) * conservation_lvl:
                                 result_dict[str(codon)].append(y.idxmax())
 
     return result_dict
 
 
-def genecode_plot(res_list_dict):
+def genecode_plot(res_list_dict, all_codons):
     std_code = CodonTable.unambiguous_dna_by_id[1].forward_table
     std_code['TAG'] = "*"
     std_code['TAA'] = "*"
@@ -148,16 +154,29 @@ def genecode_plot(res_list_dict):
                     most_freq = (count, i)
                 result[i] = res_list.count(i)
                 res = pd.Series(result)
-            if (codon in std_code) and (most_freq[1] != std_code[codon]):
+            if all_codons == True:
+                if (codon in std_code) and (most_freq[1] != std_code[codon]):
+                    print(f"Suspicious codon:{codon.replace('T','U')} doens't seem to match The standart genetic code: {std_code[codon]}")
                 final_result = res.sort_values(ascending=False)
                 final_result.plot.bar()
                 plt.title(f"{codon.replace('T','U')} (The Standard Code: {std_code[codon]})")
                 plt.xticks(rotation='horizontal')
                 pdf.savefig(bbox_inches='tight')
                 plt.close()
+            else:
+                if (codon in std_code) and (most_freq[1] != std_code[codon]):
+                    print(f"Suspicious codon:{codon.replace('T','U')} doens't seem to match The standart genetic code: {std_code[codon]}")
+                    final_result = res.sort_values(ascending=False)
+                    final_result.plot.bar()
+                    plt.title(f"{codon.replace('T','U')} (The Standard Code: {std_code[codon]})")
+                    plt.xticks(rotation='horizontal')
+                    pdf.savefig(bbox_inches='tight')
+                    plt.close()
 
 
 def main(args):
+    if args.prepare_alignments:
+        prepare_alignments(args.threads)
     transcriptome = args.input
     folder = f"{transcriptome.split('.')[0]}_genecode"
     os.mkdir(folder)
@@ -165,9 +184,11 @@ def main(args):
     os.chdir(folder)
     queries_list = args.queries.split(',')
     collect_queries(queries_list)
-    blastDB_tblastn(transcriptome, args.threads)
-    res_list_dict = stops_against_alignment(transcriptome)
-    genecode_plot(res_list_dict)
+    print(f'{"="*20}\nPerforming blast searches ...\n{"="*20}\n')
+    blastDB_tblastn(transcriptome, args.threads, args.blast_evalue)
+    print(f'{"="*20}\nCollecting information from conserved positions ...\n{"="*20}\n')
+    stats = collect_counts(transcriptome, args.conserved)
+    genecode_plot(stats, args.all_codons)
 
 if __name__ == '__main__':
     config = configparser.ConfigParser()
@@ -177,9 +198,7 @@ if __name__ == '__main__':
     description = 'Script for fast genetic code analysis.'
     parser, optional, required = help_formatter.initialize_argparse(name='genetic_code.py',
                                                                     desc=description,
-                                                                    usage='genetic_code.py [OPTIONS]',
-                                                                    dataset=False,
-                                                                    input_meta=False)
+                                                                    usage='genetic_code.py [OPTIONS]')
 
     # Required Arguments
     required.add_argument('-i', '--input', required=True, type=str, metavar='file.fas',
@@ -202,9 +221,19 @@ if __name__ == '__main__':
                 Prepare alignments for genetic code analysis.
                 """))
 
-    optional.add_argument('-c', '--conserved', type=int, metavar='N',
+    optional.add_argument('-c', '--conserved', type=float, metavar='0-1',default=0.7,
                 help=textwrap.dedent("""\
             Conservation level. 0-1.
+            """))
+
+    optional.add_argument('-e', '--blast_evalue', type=str, metavar='1e-X',default='1e-30',
+                help=textwrap.dedent("""\
+            Evalue for blast searches. Default: 1e-30.
+            """))
+
+    optional.add_argument('-a', '--all_codons', action='store_true', default=False,
+                help=textwrap.dedent("""\
+            Plot conserved positions for all codons.
             """))
 
     args = help_formatter.get_args(parser, optional, required, pre_suf=False, inp=False)
