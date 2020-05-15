aaa0f88082981ed49564b71749f8cf5b5cfe4370
Robert Jones <rej110@msstate.edu>
2020-05-15 12:28:04 -0500

    refractored

--- phylofisher/utilities/fast_site_removal.py
+++ phylofisher/utilities/fast_site_removal.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python
 import os
 import subprocess
+import sys
 import textwrap
 import string
 import random
 from Bio import SeqIO
 import pandas as pd
+from Bio.Alphabet import IUPAC
+from Bio.Seq import Seq
+from Bio.SeqRecord import SeqRecord
 
 from phylofisher import help_formatter
 
@@ -65,8 +69,7 @@ ub = 10.0              * upper bound for rates"""
 
 
 def run_dist():
-    dist_cmd = '/mnt/scratch/brownlab/rej110/PhyloFisher/git_repo/lib/dist_estv1.1/dist_est'
-    cmd = f'{dist_cmd} dist_est.ctl'
+    cmd = 'dist_est dist_est.ctl'
     subprocess.run(cmd, shell=True)
 
 
@@ -82,11 +85,28 @@ def parse_rates():
     return result
 
 
+def write_seqs(out_handle, records):
+    # Accepted out formats with respective suffix
+    out_dict = {'fasta': 'fas',
+                'phylip': 'phy',
+                'phylip-relaxed': 'phy',
+                'nexus': 'nex'}
+
+    # Writes to output matrix in user specified output
+    if args.out_format.lower() in out_dict:
+        SeqIO.write(records, out_handle, args.out_format.lower())
+    else:
+        sys.exit('Invalid Output Format')
+
+
 def main():
     pseudo_, pseudo_rev_ = fake_phylip(args.matrix)
     fake_tree(args.tree, pseudo_)
     control_file()
-    run_dist()
+    # Checks to see if dist_est has been run. If not do so
+    if not os.path.isfile('./rate_est.dat'):
+        run_dist()
+
     matrix_dict = {}
     for record in SeqIO.parse(args.matrix, 'fasta'):
         matrix_dict[record.name] = pd.Series(list(record.seq))
@@ -97,15 +117,22 @@ def main():
     os.chdir(f'chunks_{args.chunk}')
     for chunk in range(args.chunk, len(sorted_rates), args.chunk):
         with open(f'chunk{iter}', 'w') as res:
+            records = []
             for name, seq in matrix_dict.items():
-                res.write(f'>{name}\n{"".join(seq[sorted_rates[chunk:]].values)}\n')
+                seq = "".join(seq[sorted_rates[chunk:]].values)
+                records.append(SeqRecord(Seq(seq, IUPAC.protein),
+                                         id=name,
+                                         name='',
+                                         description=''))
+            write_seqs(res, records)
+
         iter += 1
     os.remove('../TEMP.phy')
     os.remove('../TEMP.tre')
 
 
 if __name__ == "__main__":
-    description = 'Script for ortholog fishing.'
+    description = 'This removes the fastest evolving sites within the phylogenomic supermatrix in a stepwise fashion.'
     parser, optional, required = help_formatter.initialize_argparse(name='fast_site_removal.py',
                                                                     desc=description,
                                                                     usage='fast_site_removal.py '
@@ -114,18 +141,26 @@ if __name__ == "__main__":
     # Required Arguments
     required.add_argument('-m', '--matrix', required=True, type=str, metavar='<matrix>',
                           help=textwrap.dedent("""\
-                          Path to matrix
-                          """))
+                              Path to matrix
+                              """))
 
     # Optional Arguments
     optional.add_argument('-tr', '--tree', type=str, metavar='',
                           help=textwrap.dedent("""\
-                          Path to tree
-                          """))
-    optional.add_argument('-c', '--chunk', type=int, default=3000,
+                              Path to tree
+                              """))
+    optional.add_argument('-c', '--chunk', type=int, default=3000, metavar='N',
                           help=textwrap.dedent("""\
-                          description of chunk
-                          """))
+                              Size of removal step (i.e., 1000 sites removed) to exhaustion
+                              Default: 3000
+                              """))
+    optional.add_argument('-f', '--out_format', metavar='<format>', type=str, default='phylip-relaxed',
+                          help=textwrap.dedent("""\
+                              Desired format of the output chunks.
+                              Options: fasta, nexus, phylip (names truncated at 10 characters), 
+                              or phylip-relaxed (names are not truncated)
+                              Default: phylip-relaxed
+                              """))
 
     args = help_formatter.get_args(parser, optional, required, pre_suf=False, inp_dir=False)
 
--- phylofisher/utilities/random_sample_iteration.py
+++ phylofisher/utilities/random_sample_iteration.py
@@ -1,58 +1,156 @@
-"""
-Useage:
-python makeIQ.py
-"""
-import os
-import sys
+#!/usr/bin/env python
 import glob
+import os
+import shutil
+import textwrap
+import numpy
+import math
 import random
-totalgenes = int(sys.argv[1]) #### How many total genes do you have?
-iterations = int(sys.argv[2]) #### How many iterations would you like to run
-tenpercent = totalgenes*0.1
-twentypercent = totalgenes*0.2
-thirtypercent = totalgenes*0.3
-fortypercent = totalgenes*0.4
-fiftypercent = totalgenes*0.5
-sixtypercent = totalgenes*0.6
-seventypercent = totalgenes*0.7
-eightypercent = totalgenes*0.8
-ninetypercent = totalgenes*0.9
-genes = [fname [:-9] for fname in glob.glob('*.bmge.fas')]
-cd=os.getcwd()
-for n in range(iterations):
-os.system("rm -r twenty/ forty/ sixty/ eighty/")
-twenty = random.sample(genes, int(twentypercent))
-forty = random.sample(genes, int(fortypercent))
-sixty = random.sample(genes, int(sixtypercent))
-eighty = random.sample(genes, int(eightypercent))
-os.system("mkdir twenty")
-for i in twenty:
-os.system("cp %s.bmge.fas twenty" %(i))
-os.system("cp alvert_septable.py seqtools.py twenty")
-os.chdir('twenty')
-os.system("python alvert_septable.py -c bmge.fas Bordor.20.%s.dat" %(n))
-os.system("cp *.dat* ../")
-os.chdir(cd)
-os.system("mkdir forty")
-for i in forty:
-os.system("cp %s.bmge.fas forty" %(i))
-os.system("cp alvert_septable.py seqtools.py forty")
-os.chdir('forty')
-os.system("python alvert_septable.py -c bmge.fas Bordor.40.%s.dat" %(n))
-os.system("cp *.dat* ../")
-os.chdir(cd)
-os.system("mkdir sixty")
-for i in sixty:
-os.system("cp %s.bmge.fas sixty" %(i))
-os.system("cp alvert_septable.py seqtools.py sixty")
-os.chdir('sixty')
-os.system("python alvert_septable.py -c bmge.fas Bordor.60.%s.dat" %(n))
-os.system("cp *.dat* ../")
-os.chdir(cd)
-os.system("mkdir eighty")
-for i in eighty:
-os.system("cp %s.bmge.fas eighty" %(i))
-os.system("cp alvert_septable.py seqtools.py eighty")
-os.chdir('eighty')
-os.system("python alvert_septable.py -c bmge.fas Bordor.80.%s.dat" %(n))
-os.system("cp *.dat* ../")
+import tarfile
+import pandas as pd
+from phylofisher import help_formatter
+
+
+def make_tmp_dir(base):
+    """
+    Makes tmpo directory. If it exists the old tmp directory is first removed
+    """
+    tmp_dir = f'{base}/tmp'
+    try:
+        os.mkdir(tmp_dir)
+    except FileExistsError:
+        shutil.rmtree(tmp_dir)
+        os.mkdir(tmp_dir)
+
+
+def get_num_of_replicates(x):
+    """
+    Uses 0.95=1-(1-x/100)^n to determine the number of replicates where x is the percentage of genes samples and
+    n is the number of replicates
+    """
+    bottom_eq = 1 - (x / 100)
+    confidence = 1 - args.confidence_interval
+    n = numpy.log(confidence) / numpy.log(bottom_eq)
+    n = math.ceil(n)
+    return n
+
+
+def subsample(percent, genes, output):
+    """
+    Creates gene subsets and super matirices of the subsampled gene sets.
+    """
+    replicate_num = get_num_of_replicates(percent)
+    # Creates gene subsample sets with the number of replicates calculated in get_num_of_replicates
+    for i in range(replicate_num):
+        size = len(genes) * percent * 0.01
+        sample = random.sample(genes, int(size))
+        make_tmp_dir(output)
+        # copy gene files into a tmp directory
+        for file in sample:
+            shutil.copy(file, f'{output}/tmp')
+        # Runs forge to create super matrices of subsampled gene sets
+        os.system(f'forge.py -i {output}/tmp -o {output}/replicate_{i + 1} -f {args.out_format.lower()}')
+        shutil.rmtree(f'{output}/tmp')
+
+
+def make_csv():
+    perc_dict = dict()
+    for x in sampling_percents:
+        perc_dict[str(x)] = dict()
+    for root, dirs, files in os.walk(args.output):
+        for file in files:
+            if file == 'indices.tsv':
+                path = os.path.join(root, file)
+                _, perc, rep, _ = path.split('/')
+                perc, _ = perc.split('_')
+                _, rep = rep.split('_')
+
+                with open(path, 'r') as infile:
+                    perc_dict[str(perc)][rep] = []
+                    infile.readline()
+                    for line in infile:
+                        line = line.strip()
+                        gene, _, _ = line.split('\t')
+                        perc_dict[str(perc)][rep].append(gene)
+
+    for percent in perc_dict.keys():
+        data = perc_dict[percent]
+        data = {int(k): v for k, v in data.items()}
+        df = pd.DataFrame(data).transpose()
+        df = df.sort_index().transpose()
+        df = df.add_prefix('Replicate_')
+
+        df.to_csv(f'{args.output}/{percent}_Percent.csv', index=False)
+
+
+def clean_up():
+    """
+    Copies matricies in to root output directory and tar's "percent" directories
+    """
+    for root, dirs, files in os.walk(args.output):
+        for file in files:
+            if file.endswith('.fas') or file.endswith('.phy'):
+                path = os.path.join(root, file)
+                ext = path.split('.')[-1]
+                _, perc, rep, _ = path.split('/')
+                perc, _ = perc.split('_')
+                _, rep = rep.split('_')
+
+                new_name = f'{args.output}/{perc}rep{rep}.{ext}'
+                shutil.copy(path, new_name)
+
+    os.chdir(args.output)
+    for root, dirs, files in os.walk('.'):
+        for dir in dirs:
+            if dir.endswith('Percent'):
+                with tarfile.open(f'{dir}.tar.gz', mode='x:gz') as my_tar:
+                    my_tar.add(dir)
+                shutil.rmtree(dir)
+
+
+if __name__ == '__main__':
+    description = 'Constructs super-matrices from randomly sampled genes.'
+    parser, optional, required = help_formatter.initialize_argparse(name='random_sample_iteration.py',
+                                                                    desc=description,
+                                                                    usage='random_sample_iteration.py '
+                                                                          '[OPTIONS] -i /path/to/input/')
+
+    # Optional Arguments
+    optional.add_argument('-f', '--out_format', metavar='<format>', type=str, default='phylip-relaxed',
+                          help=textwrap.dedent("""\
+                              Desired format of the output chunks.
+                              Options: fasta, nexus, phylip (names truncated at 10 characters), 
+                              or phylip-relaxed (names are not truncated)
+                              Default: phylip-relaxed
+                              """))
+    optional.add_argument('-ci', '--confidence_interval', metavar='0.N', type=float, default=0.95,
+                          help=textwrap.dedent("""\
+                              Confidence interval to use to calculate the number of replicates required.
+                              Default: 0.95%%
+                              """))
+    optional.add_argument('-ps', '--percent_sampling', metavar='N', type=int, default=20,
+                          help=textwrap.dedent("""\
+                              Percent sampling step size.
+                              Default: 20%% (e.g. 5%%, 10%%, 20%%, 25%%)
+                              The default 20%% sampling results in a sampling series of 20%%, 40%%, 60%%, and 80%%.
+                              """))
+
+    in_help = 'Path to input directory containing gene files in FASTA format.'
+    args = help_formatter.get_args(parser, optional, required, in_help=in_help)
+
+    args.output = f'{args.output}_{args.percent_sampling}ps_{args.confidence_interval}ci'
+
+    os.mkdir(args.output)
+    # creates a list of percents to sample gene set at
+    sampling_percents = list(range(0, 100, args.percent_sampling))[1:]
+    # gets list of all gene files in the input directory
+    files = [file for file in glob.glob(f'{args.input}/{args.prefix}*{args.suffix}')]
+
+    # iterated through list of sampling percentages and creates a directory to store the outputs
+    for x in sampling_percents:
+        new_dir = f'{args.output}/{str(x)}_Percent'
+        os.mkdir(new_dir)
+        subsample(x, files, new_dir)
+
+    make_csv()
+    clean_up()
