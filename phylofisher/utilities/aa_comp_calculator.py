dcb55e3a126ff351a8dec7af3ffdf97883dacafe
Robert Jones <rej110@msstate.edu>
2020-06-17 15:02:44 -0500

    additional output of tree in newick format

--- phylofisher/utilities/aa_comp_calculator.py
+++ phylofisher/utilities/aa_comp_calculator.py
@@ -8,6 +8,32 @@ from Bio.SeqUtils.ProtParam import ProteinAnalysis
 import matplotlib.pyplot as plt
 import scipy.cluster.hierarchy as shc
 from phylofisher import help_formatter
+from ete3 import Tree
+
+def distance_matrix2tree(Z, names):
+    """Return tree representation for distance matrix"""
+    n = Z.shape[0]+1
+    i2n = [0] * (2*n - 1)
+    t = Tree()
+    for i, (idx1, idx2, dist, sample_count) in enumerate(Z):
+        idx1, idx2 = int(idx1), int(idx2)
+        # create Tree object for tips / leaves
+        if idx1 < n:
+            i2n[idx1] = Tree(name=names[idx1])
+        if idx2 < n:
+            i2n[idx2] = Tree(name=names[idx2])
+        # create new node
+        t = Tree()
+        # normalise distance
+        dist1 = dist - i2n[idx1].get_farthest_leaf()[1]
+        dist2 = dist - i2n[idx2].get_farthest_leaf()[1]
+        # add children
+        t.add_child(i2n[idx1], dist=dist1)
+        t.add_child(i2n[idx2], dist=dist2)
+        # store
+        i2n[n + i] = t
+    return t
+
 
 
 def make_plot():
@@ -18,7 +44,10 @@ def make_plot():
     plt.figure(figsize=(25, 10))
     plt.title("AA Composition Hierarchical Clustering")
     plt.xticks(rotation=90)
-    shc.dendrogram(shc.linkage(df, method='ward'), labels=df.index.values, color_threshold=0)
+    z = shc.linkage(df, method='ward')
+    t = distance_matrix2tree(z, df.index.values)
+    t.write(outfile='distance_matrix.tre')
+    shc.dendrogram(z, labels=df.index.values, color_threshold=0)
     plt.tight_layout()
     plt.savefig(f'AA_Composition_Hierarchical_Clustering.pdf')
 
@@ -32,8 +61,12 @@ if __name__ == '__main__':
 
     required.add_argument('-i', '--input', required=True, type=str, metavar='matrix',
                           help=textwrap.dedent("""\
-                                  Path to input matrix for analysis.
-                                  """))
+                          Path to input matrix for analysis.
+                          """))
+    # required.add_argument('-f', '--input_format', required=True, type=str, metavar='matrix',
+    #                       help=textwrap.dedent("""\
+    #                       Path to input matrix for analysis.
+    #                       """))
 
     args = help_formatter.get_args(parser, optional, required, pre_suf=False, inp_dir=False)
 
@@ -43,7 +76,7 @@ if __name__ == '__main__':
         outfile.write('Taxon\t'+'\t'.join(peptides) + '\n')
 
         # Reads in input file
-        for record in SeqIO.parse(infile, format='fasta'):
+        for record in SeqIO.parse(infile, format='phylip-relaxed'):
             outfile.write(f'{record.id}\t')
             analysed_seq = ProteinAnalysis(str(record.seq))
             count_dict = analysed_seq.count_amino_acids()
