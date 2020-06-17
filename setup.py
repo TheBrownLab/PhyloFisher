2733460bf230909216322d553ebf7f62d55518b9
Robert Jones <rej110@msstate.edu>
2020-06-17 14:49:15 -0500

    Updated to reflect moved scripts

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='phylofisher',
-    version='0.1',
-    packages=['phylofisher'],
+    version='0.1.14',
+    packages=find_packages(),
     scripts=['phylofisher/fisher.py',
              'phylofisher/config.py',
              'phylofisher/fishing_net.py',
@@ -13,21 +13,32 @@ setup(
              'phylofisher/lumberjack.py',
              'phylofisher/purge.py',
              'phylofisher/build_dataset.py',
+             'phylofisher/install_deps.py',
              'phylofisher/single_gene_tree_constructor.py',
-             'phylofisher/utilities/missing_data.py',
+             'phylofisher/missing_data.py',
              'phylofisher/utilities/fast_site_removal.py',
+             'phylofisher/utilities/mammal_modeler.py',
              'phylofisher/utilities/bipartition_examiner.py',
              'phylofisher/utilities/fast_tax_removal.py',
-             'phylofisher/utilities/heteroevolving_sites.py',
              'phylofisher/utilities/aa_comp_calculator.py',
              'phylofisher/utilities/SR4_class_recoder.py',
              'phylofisher/utilities/taxon_collapser.py',
-             'phylofisher/utilities/genetic_code.py'
+             'phylofisher/utilities/genetic_code.py',
+             'phylofisher/utilities/heterotachy.py',
+             'phylofisher/utilities/random_sample_iteration.py'
              ],
-
-    url='',
+    python_requires='>=3.7',
+    install_requires=['biopython==1.76',
+                      'pyqt5>=5',
+                      'ete3==3.1.1',
+                      'pandas==1.0.3',
+                      'matplotlib==3.1.3',
+                      'scipy'
+                      ],
+    url='https://github.com/TheBrownLab/PhyloFisher',
     license='MIT',
-    author='david',
+    author='David Zihala',
     author_email='zihaladavid@gmail.com',
-    description=''
+    description='PhyloFisher is a software package for the creation, analysis, and visualization of phylogenomic '
+                'datasets that consist of protein sequences from eukaryotic organisms.'
 )
