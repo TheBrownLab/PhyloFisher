4f4bb87d67bb325468825ef8ba0cdb597fd2fbdb
David Zihala <zihaladavid@gmail.com>
2021-02-20 15:13:45 +0100

    illegal chrs, unique names

--- phylofisher/config.py
+++ phylofisher/config.py
@@ -1,9 +1,52 @@
 #!/usr/bin/env python
+import sys
+import csv
 import configparser
 import textwrap
 from pathlib import Path
-
 from phylofisher import help_formatter
+
+
+def check_input_meta(dataset_fold, input_meta):
+    """
+    Checks that all ids are unique in input_metadata and also between
+    input_metadata and metadata. 
+    Checks for illegal characters in input ids.
+    """
+
+    database_metadata_ids = set()
+    with open(str(Path(dataset_fold, 'metadata.tsv')), 'r') as csvfile:
+        reader = csv.reader(csvfile, delimiter='\t')
+        for row in reader:
+            id_ = row[0]
+            if id_ != 'Unique ID':
+                database_metadata_ids.add(id_)
+
+    input_ids = set()
+    with open(input_meta, 'r') as csvfile:
+        reader = csv.reader(csvfile, delimiter='\t')
+        for row in reader:
+            id_ = row[2]
+            if id_ == 'Unique ID':
+                continue
+            
+            #check that ID is unique
+            if id_ in input_ids:
+                print("ERROR: ", id_, "is not a unique ID")
+                sys.exit()
+
+            if id_ in database_metadata_ids:
+                print("ERROR: ", id_, "already in the database")
+                sys.exit()
+
+            #check illegal characters
+            for ch in ["_", '@', '..', '*', ' ']:
+                if ch in id_:
+                    print(f"ERROR: illegal character {ch} in {id_}")
+                    sys.exit()
+
+            input_ids.add(id_)
+
 
 if __name__ == '__main__':
     description = 'Script for the analysis folder configuration.'
@@ -33,6 +76,9 @@ if __name__ == '__main__':
 
     config = configparser.ConfigParser()
     args = help_formatter.get_args(parser, optional, required, pre_suf=False, inp_dir=False, out_dir=False)
+
+
+    check_input_meta(args.database_folder, args.input_file)
 
     if not args.orthomcl:
         args.orthomcl = str(Path(args.database_folder, 'orthomcl'))
