7a0f3e7f3b697a0dd9b0da0a565dd6e5d6fe654c
Robert E. Jones <robert.ervin.jones@gmail.com>
2021-08-05 22:44:55 -0500

    updates for v1.0.10

--- phylofisher/help_formatter.py
+++ phylofisher/help_formatter.py
@@ -3,7 +3,7 @@ import textwrap
 from datetime import date
 
 
-version = '1.0.9'
+version = '1.0.10'
 today = date.today()
 
 
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='phylofisher',
-    version='1.0.9',
+    version='1.0.10',
     packages=find_packages(),
     scripts={'phylofisher/fisher.py',
              'phylofisher/config.py',
