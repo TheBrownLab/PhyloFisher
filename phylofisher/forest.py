0bdf5a4c39cd70665b268357f8cf2bad7954e23a
Eric Salomaki <eric.salomaki@gmail.com>
2022-02-24 16:48:52 +0100

    Create forest.py

--- phylofisher/forest.py
+++ phylofisher/forest.py
@@ -400,7 +400,9 @@ def parallel_susp_clades(trees):
             suspicious = list(pool.map(suspicious_clades, trees))
             return suspicious
     else:
-        suspicious = [suspicious_clades(t) for t in trees]
+        suspicious = []
+        for tree in trees:
+            suspicious.append(suspicious_clades(tree))
         return suspicious
 
 
