5beaba3994f0d935a9de88aa81692c63404aaf1d
Robert Jones <robert.ervin.jones@gmail.com>
2022-09-09 10:45:58 -0500

    updates comments

--- phylofisher/utilities/taxon_collapser.py
+++ phylofisher/utilities/taxon_collapser.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-from ast import Raise
 import configparser
 import os
 import shutil
@@ -27,12 +26,15 @@ def parse_collapse_tsv():
     with open(args.input, 'r') as infile:
         collapse_dict = dict()
         for line in infile:
+            # Check to make sure the correct number of columns are present
             try:
-                chimera_id, higher, lower, to_collapse = line.strip().split('t')
+                chimera_id, long_name, higher, lower, to_collapse = line.strip().split('\t')
             except ValueError:
-                Raise('Error parsing to_collapse.tsv. Please make sure the file is formatted correctly.')
-                
+                raise Exception('Error parsing to_collapse.tsv. Please make sure the file is formatted correctly.')
+            
+            # Add parsed information to dictionary
             collapse_dict[chimera_id] = {
+                'long_name': long_name,
                 'higher': higher,
                 'lower': lower,
                 'to_collapse': to_collapse.split(',')
@@ -41,58 +43,68 @@ def parse_collapse_tsv():
     return collapse_dict
 
 
-def check_metadata():
-    """
+def check_metadata(collapse_dict):
+    '''
     Checks to make sure taxa provided in to_collapse.csv are actually in the meta data
-    """
-    with open(args.input, 'r') as infile:
-        to_collapse = set()
-        for taxon in [line.strip().split(',')[2:] for line in infile]:
-            to_collapse.update(taxon)
 
-    collapse_dict = dict.fromkeys(to_collapse, False)
+    :param collapse_dict: information about taxa to collapse
+    :type collapse_dict: dict
+    '''
+    to_collapse = [taxon for k in collapse_dict.keys() for taxon in collapse_dict[k]['to_collapse']]
+    taxon_dict = dict.fromkeys(to_collapse, False)
 
+    # Loop through metadata and check if taxa are in metadata
     with open(metadata, 'r') as infile:
         for line in infile:
             line = line.strip()
-
             short_name = line.split('\t')[0]
             if short_name in to_collapse:
-                collapse_dict[short_name] = True
+                taxon_dict[short_name] = True
 
-    for taxon in collapse_dict.keys():
-        if collapse_dict[taxon] is False:
+    # Check if all taxa are in metadata
+    for taxon in taxon_dict.keys():
+        if taxon_dict[taxon] is False:
             print(f'{taxon} is not in the dataset.')
 
-    if False in collapse_dict.values():
-        sys.exit('Taxa not collapsed. Please fix errors above.')
+    # Raise error if any taxa are not in metadata
+    if False in taxon_dict.values():
+        raise Exception('Taxa not collapsed. Please fix errors above.')
 
 
-def add_to_meta(abbrev, input_info):
-    """"Transfer input metadata for a given organism
-    to dataset metadata.
-    input:  short name of organism
-    return: None
-    """
+def add_to_meta(collapse_dict):
+    '''
+    Transfer input metadata for a given organism to dataset metadata.
 
-    new_row = {'Unique ID': abbrev,
-               'Long Name': input_info[abbrev][0],
-               'Higher Taxonomy': input_info[abbrev][1],
-               'Lower Taxonomy': input_info[abbrev][2],
-               'Source': 'taxon_collapser.py',
-               'Data Type': 'Collapsed'
-               }
+    :param collapse_dict: information about taxa to collapse
+    :type collapse_dict: dict
+    '''
+    for chimera_id in collapse_dict.keys():
+        # Create new row dictionary
+        new_row = {'Unique ID': chimera_id,
+                'Long Name': collapse_dict[chimera_id]['long_name'],
+                'Higher Taxonomy': collapse_dict[chimera_id]['higher'],
+                'Lower Taxonomy': collapse_dict[chimera_id]['lower'],
+                'Source': 'taxon_collapser.py',
+                'Data Type': 'Collapsed'
+                }
 
-    df = pd.read_csv(metadata, delimiter='\t')
-    df.index = df['Unique ID']
+        # Read metadata into dataframe
+        df = pd.read_csv(metadata, delimiter='\t')
+        df.index = df['Unique ID']
 
-    df = df.append(new_row, ignore_index=True)
-    df = df.sort_values(by=['Higher Taxonomy', 'Lower Taxonomy', 'Unique ID'])
-    df.to_csv(str(Path(dfo, 'metadata.tsv')), sep='\t', index=False)
+        # Add new row to dataframe
+        df = df.append(new_row, ignore_index=True)
+        df = df.sort_values(by=['Higher Taxonomy', 'Lower Taxonomy', 'Unique ID'])
+        df.to_csv(str(Path(dfo, 'metadata.tsv')), sep='\t', index=False)
 
 
-def collapse():
-    collapse_dict = parse_collapse_tsv()
+def collapse(collapse_dict):
+    '''
+    Collapses sequences in the dataset.
+
+    :param collapse_dict: information about taxa to collapse
+    :type collapse_dict: dict
+    '''
 
     # Collapse Ortholgogs
     ortho_dir = f'{dfo}/orthologs'
@@ -102,18 +114,21 @@ def collapse():
         records = []
         with open(gene_file, 'r') as infile:
             for record in SeqIO.parse(infile, 'fasta'):
-                for key in collapse_dict.keys():
-                    if record.description in collapse_dict[key]:
-                        if len(record.seq) > len(record_dict[key].seq):
-                            record_dict[key].id = key
-                            record_dict[key].seq = record.seq
+                for chimera_id in collapse_dict.keys():
+                    if record.description in collapse_dict[chimera_id]['to_collapse']:
+                        if len(record.seq) > len(record_dict[chimera_id].seq):
+                            record_dict[chimera_id].id = chimera_id
+                            record_dict[chimera_id].seq = record.seq
 
+                # Append original sequences to records
                 records.append(record)
 
+        # Append collapsed sequences to records
         for record in record_dict.values():
             if str(record.seq) != '':
                 records.append(record)
 
+        # Write ortholog file post collapse
         with open(gene_file, 'w') as outfile:
             SeqIO.write(records, outfile, 'fasta')
 
@@ -126,19 +141,19 @@ def collapse():
         records = []
         with open(gene_file, 'r') as infile:
             for record in SeqIO.parse(infile, 'fasta'):
-                for key in collapse_dict.keys():
-                    if record.description.split('..')[0] in collapse_dict[key]:
-                        records.append(SeqRecord(record.seq, id=f'{key}..{record.description.split("..")[1]}', name='', description=''))
+                for chimera_id in collapse_dict.keys():
+                    if record.description.split('..')[0] in collapse_dict[chimera_id]['to_collapse']:
+                        records.append(SeqRecord(record.seq, id=f'{chimera_id}..{record.description.split("..")[1]}', name='', description=''))
                 
                 records.append(record)
 
+        # Write ortholog file post collapse
         with open(gene_file, 'w') as outfile:
             SeqIO.write(records, outfile, 'fasta')
 
         record_dict.clear()
 
-    for key in collapse_dict.keys():
-        add_to_meta(key, collapse_dict)
+    add_to_meta(collapse_dict)
 
 
 if __name__ == '__main__':
@@ -149,12 +164,12 @@ if __name__ == '__main__':
 
     required.add_argument('-i', '--input', type=str, metavar='to_collapse.tsv', default=None,
                           help=textwrap.dedent("""\
-                          A .tsv containing a Unique ID, higher taxonomy, lower taxonomy, and a list of Unique IDs to collapse
+                          A .tsv containing a Unique ID, long name, higher taxonomy, lower taxonomy, and a list of Unique IDs to collapse
                           for each chimera with one on each line.
                           
                           Example:
-                            Chimera ID 1\tHigher Taxonomy\tLower Taxonomy\tUnique ID 1,Unique ID 2
-                            Chimera ID 2\tHigher Taxonomy\tLower Taxonomy\tUnique ID 3,Unique ID 4
+                            Chimera_ID 1\tLong Name\tHigher_Tax\tLower_Tax\tUnique_ID_1,Unique_ID_2
+                            Chimera_ID 2\tLong Name\tHigher_Tax\tLower_Tax\tUnique_ID_3,Unique_ID_4
                         """))
     args = help_formatter.get_args(parser, optional, required,
                                    out_dir=False, pre_suf=False, inp_dir=False)
@@ -166,5 +181,6 @@ if __name__ == '__main__':
     metadata = str(os.path.join(dfo, 'metadata.tsv'))
 
     tools.backup(dfo)
-    check_metadata()
-    collapse()
+    collapse_dict = parse_collapse_tsv()
+    check_metadata(collapse_dict)
+    collapse(collapse_dict)
